85b2db01e72f6ccd28a9c8b492ddc9e7e9aae748
David Eriksson <david.eriksson@valtech.com>
2022-12-02 11:27:04 +0100

    feat: day 2

---
+++ day2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"input"
+	"strings"
+)
+
+
+func main () {
+  total := 0
+  lines := strings.Split(input.Get("2"), "\n")
+
+  m := map[string][]int{
+    "A X": {4, 3},
+    "A Y": {8, 4},
+    "A Z": {3, 8},
+    "B X": {1, 1},
+    "B Y": {5, 5},
+    "B Z": {9, 9},
+    "C X": {7, 2},
+    "C Y": {2, 6},
+    "C Z": {6, 7},
+  }
+
+  for _, line := range lines {
+    if len(line) == 0 {
+      break // Last line of input is not formatted correctly
+    }
+    total += m[line][0]
+  }
+  fmt.Println(total)
+}
---
+++ day2_2/go.mod
@@ -0,0 +1,9 @@
+module day2_2
+
+go 1.19
+
+require input v1.0.0
+
+require github.com/joho/godotenv v1.4.0 // indirect
+
+replace input v1.0.0 => ../input
