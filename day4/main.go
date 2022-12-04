f91cf3f17c83f27c927f7c8f8d9e2d4f45859917
David Eriksson <david.eriksson@valtech.com>
2022-12-04 14:48:01 +0100

    feat: day 4

---
+++ day4/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"input"
+)
+
+func main () {
+  lines := strings.Split(input.Get("4"), "\n")
+  total := 0
+
+  for i := 0; i < len(lines); i++ {
+    if len(lines[i]) == 0 {
+      break // Last line of input is not formatted correctly
+    }
+
+    pair := strings.Split(lines[i], ",")
+    first := strings.Split(pair[0],"-")
+    second := strings.Split(pair[1],"-")
+
+    first_min, _ := strconv.Atoi(first[0])
+    first_max, _ := strconv.Atoi(first[1])
+    second_min, _ := strconv.Atoi(second[0])
+    second_max, _ := strconv.Atoi(second[1])
+
+    if contained(first_min, first_max, second_min, second_max) {
+      total += 1
+    } else if contained(second_min, second_max, first_min, first_max) {
+      total += 1
+    }
+  }
+  fmt.Println(total)
+}
+
+
+func contained (min1 int, max1 int, min2 int, max2 int) bool {
+  if min1 >= min2 && max1 <= max2 {
+    return true
+  } else {
+    return false
+  }
+}
---
+++ day4_2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"input"
+)
+
+func main () {
+  lines := strings.Split(input.Get("4"), "\n")
+  total := 0
+
+  for i := 0; i < len(lines); i++ {
+    if len(lines[i]) == 0 {
+      break // Last line of input is not formatted correctly
+    }
+
+    pair := strings.Split(lines[i], ",")
+    first := strings.Split(pair[0],"-")
+    second := strings.Split(pair[1],"-")
+
+    first_min, _ := strconv.Atoi(first[0])
+    first_max, _ := strconv.Atoi(first[1])
+    second_min, _ := strconv.Atoi(second[0])
+    second_max, _ := strconv.Atoi(second[1])
+
+    if overlapping(first_min, first_max, second_min, second_max) {
+      total += 1
+    }
+  }
+  fmt.Println(total)
+}
+
+func overlapping (min1 int, max1 int, min2 int, max2 int) bool {
+  if max1 >= min2 && min1 <= max2 {
+    return true
+  } else {
+    return false
+  }
+}
